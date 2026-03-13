53720113474971999c14fec4b462204bcb24908b
Nico <riveraent05@gmail.com>
2026-03-13 15:24:44 +0800

    Delete Timekeepers-Suitcase directory

--- Timekeepers-Suitcase/CMS/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    <footer>
-      <div class="container">
-        &copy; Timekeeper's Suitcase <?= date('Y'); ?>
-      </div>
-    </footer>
-    <script src="js/site.js"></script>
-  </body>
-</html>
